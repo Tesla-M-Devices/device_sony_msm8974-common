5334b813a8d75260526606c6615492a18a7c85c2
Chirayu Desai <chirayudesai1@gmail.com>
2016-02-09 23:25:43 +0530

    Build and use Sony camera HAL
    Change-Id: I9dc7ce51efd947017fbc02e952564fbb61d12dc2
    
    Conflicts:
    	msm8974.mk

--- msm8974.mk
+++ msm8974.mk
@@ -34,9 +34,24 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf
 
+<<<<<<< HEAD
 # Boringssl-compat
 PRODUCT_PACKAGES += \
     libboringssl-compat
+=======
+# Camera
+PRODUCT_PACKAGES += \
+    camera.msm8974 \
+    libmmcamera_interface \
+    libmmjpeg_interface \
+    libmm-qcamera \
+    libqomx_core
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    camera.disable_zsl_mode=0 \
+    persist.camera.HAL3.enabled=1 \
+    persist.camera.ois.disable=0
+>>>>>>> 1b863fc... Build and use Sony camera HAL
 
 # Compatibility with older blobs
 PRODUCT_PACKAGES += \
